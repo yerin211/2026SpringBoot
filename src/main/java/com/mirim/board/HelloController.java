1caa8ed4e3ace8eec8c5ebc1eceb45727afb970f
yerin211 <@yerin09211@gmail.com>
2026-08-27 19:55:21 +0900

    [feat] : 첫번째 Spring Boot

--- src/main/java/com/mirim/board/HelloController.java
+++ src/main/java/com/mirim/board/HelloController.java
@@ -1,12 +1,43 @@
 package com.mirim.board;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+//Controller 는 문자가 아니라 View(화면 이름)로 인식한다
+//RestController 는 화면 이름이 아니라 그 자체로 인기
 @RestController
 public class HelloController {
+
+    //브라우저 -> 내장 톰캣(서버) -> 교통 정리 담당 -> HelloController.hello()
+    //교통정리 담당 = DispatcherServlet
+    //컨트롤러로 보냄 ex.HelloController
+    //DispatcherServlet이 하는 일
+    // - 주소를 보고 어느 메서드로 보낼지 고른다. ex./hello
+    // - 목적지가 없다면 404를 응답한다.
+
+    //CRUD : Create / *Read(Get)* / Update / Delete
+    //브라우저에서 주소창으로 직접 요청할때는 GET 이외의 메서드는 보낼 수 없다.
+    //1. 게시글 작성하는 어떻게 테스트할까?
+    //2. RestController, GetMapping 뭐하는 애들일까?
+
+    @Value("${my.message}")
+    private String message;
+
     @GetMapping("/")
     public String hello(){
         return "hello";
+    }
+    @GetMapping("/hello")//주소 창에 /hello 추가
+    public String hello2(){
+       return message;
+//        throw new RuntimeException("일부러 에러를 냈습니다.");
+    }
+    @GetMapping("/hello-map")
+    public Map<String, Object> helloMap(){
+        return Map.of("name","김미림","grade",2);
     }
 }
--- src/main/java/com/mirim/board/PostController.java
+++ src/main/java/com/mirim/board/PostController.java
@@ -1,4 +1,26 @@
 package com.mirim.board;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts") //기본 주소값 만들기
 public class PostController {
+
+    @GetMapping
+    public String getPosts() {
+        return "게시글의 목록입니다.";
+    }
+
+    @GetMapping("/count")
+    public String getPostCount() {
+        return "게시글 개수 : 0개";
+    }
+
+    @PostMapping
+    public String createPost() {
+        return "게시글이 등록되었습니다.";
+    }
 }
