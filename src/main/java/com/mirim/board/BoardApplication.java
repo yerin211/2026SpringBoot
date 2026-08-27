ede88a16fc799194d95bc8907233988bdf28d060
yerin211 <yerin09211@gmail.com>
2026-08-27 19:55:21 +0900

    [feat] : 첫번째 Spring Boot

--- src/main/java/com/mirim/board/BoardApplication.java
+++ src/main/java/com/mirim/board/BoardApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
     public static void main(String[] args) {
+        //1. 필요한 객체들을 찾아서 만들어둡니다.
+        //2. 만들어서 창고에 저장한다. (창고 = 스프링 컨테이너)
+        //3. 내장 톰켓(서버)를 띄운다. 그에 맞는 포트(8080)가 열린다.
+        //4. 요청을 받아서 요처이 오면 알맞는 코드로 넘겨준다.
         SpringApplication.run(BoardApplication.class, args);
     }
 
